923aa9c66f13ce9da387680355a37a54f549cabc
SIST <SIST@DESKTOP-HPJFCP3>
2025-01-17 18:18:03 +0900

    16th day(25.01.16)

--- javaPro/src/days16/Child.java
+++ javaPro/src/days16/Child.java
@@ -5,7 +5,7 @@ import days17.Parent;
 public class Child extends Parent{
 
 	void modifiersTest() {
-
+		
 		// (조건) 다른 패키지 안(==패키지 외부) - Parent, Child 상속관계
 
 		// 패키지 외부여도 상속관계가 있으므로 protected 필드 보여짐
