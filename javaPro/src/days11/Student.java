6e2af8ab5c540543b4f5c45bd4d1729513538771
yusica09 <choijiwoo0922@gmail.com>
2025-01-21 12:21:34 +0900

    Student 클래스 수정(toString 오버라이딩 추가)

--- javaPro/src/days11/Student.java
+++ javaPro/src/days11/Student.java
@@ -16,5 +16,11 @@ public class Student {
 		return String.format("%s\t%d\t%d\t%d\t%d\t%.2f\t\t%d\t%d", 
 				name, kor, eng, mat, tot, avg, rank, wrank);
 	}
+	@Override
+	public String toString() {
+		 return String.format("%s\t%d\t%d\t%d\t%d\t%.2f\t\t%d", 
+				name, kor, eng, mat, tot, avg, rank);
+	}
+	
 
 }
